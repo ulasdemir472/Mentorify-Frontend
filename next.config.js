0f3799f1eb313255db12e9ff7ea5c5b5fc58507b
ulasdemir472 <ulasdemir472@gmail.com>
2024-05-29 17:13:59 +0300

    backend url added to domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.mentorcruise.com", "*", "i.pravatar.cc", "localhost"],
+    domains: [
+      "cdn.mentorcruise.com",
+      "i.pravatar.cc",
+      "localhost",
+      "mentorify-backend.onrender.com",
+    ],
   },
   env: {
     SECRET_API: process.env.SECRET_API,
